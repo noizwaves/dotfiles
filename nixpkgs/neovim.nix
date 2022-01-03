6edec651b5cd4cc84eebd86aa3340b3773d5488c
Adam Neumann <noizwaves@users.noreply.github.com>
2022-01-02 21:03:08 -0700

    Remove dead code

--- nixpkgs/neovim.nix
+++ nixpkgs/neovim.nix
@@ -252,7 +252,6 @@ in {
     -- Setup lspconfig
     local on_attach = function(client, bufnr)
       local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-      local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
       local opts = { noremap=true, silent=true }
 
