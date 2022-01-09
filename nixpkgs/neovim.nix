4f2c52833de623c9a4d7778cf311fe264da1b707
Adam Neumann <adam.neumann@gusto.com>
2022-01-09 16:48:30 -0700

    Remove solargraph LSP setup

--- nixpkgs/neovim.nix
+++ nixpkgs/neovim.nix
@@ -251,7 +251,6 @@ in {
     local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     require('lspconfig').rnix.setup { on_attach = on_attach, capabilities = capabilities }
-    require('lspconfig').solargraph.setup { on_attach = on_attach, capabilities = capabilities }
     EOF
 
     lua <<EOF
