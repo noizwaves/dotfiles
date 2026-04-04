0eb5f0900fd27b34cb95fe1fd4176e5935476dff
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2026-04-04 15:13:59 -0600

    feat: hide nvim-biscuits bracket hints on startup
    The inline bracket annotations are useful for deeply nested code but noisy most of the time. Default them off; <leader>bb still toggles them on when wanted.

--- nvim/.config/nvim/lua/plugins/biscuits.lua
+++ nvim/.config/nvim/lua/plugins/biscuits.lua
@@ -3,6 +3,9 @@ return {
     "code-biscuits/nvim-biscuits",
     opts = {
       toggle_keybind = "<leader>bb",
+      default_config = {
+        show_on_start_up = false,
+      },
     },
   },
 }
