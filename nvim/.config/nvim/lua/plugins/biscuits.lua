29a09176ded400c2033b178e71adf0c190e18370
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2026-04-04 15:13:51 -0600

    refactor: split misc.lua plugin specs into individual files
    The misc.lua grouping was a convenience bucket that obscured what was installed. One file per plugin matches the rest of lua/plugins/ and makes future edits land in predictable places.

---
+++ nvim/.config/nvim/lua/plugins/biscuits.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "code-biscuits/nvim-biscuits",
+    opts = {
+      toggle_keybind = "<leader>bb",
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,3 @@
+return {
+  { "folke/trouble.nvim", cmd = "Trouble", opts = {} },
+}
